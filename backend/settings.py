b09127504ffc0c1d1c189f2ae1ca7e1329c9a97a
chan-web-source <chan.web.work@gmail.com>
2025-11-02 22:27:07 +0800

    update on settings for db migrations

--- backend/settings.py
+++ backend/settings.py
@@ -95,6 +95,9 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PWD"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
+        "OPTIONS": {
+            "options": "-c search_path=plantmail,public"
+        }
     }
 }
 
